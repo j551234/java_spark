eb1e4e2657b1c95cb07c7fa8e9b460ae45c39f71
JamesPeng <james850704@gmail.com>
2021-01-20 15:39:01 +0800

    to rename the example for sparksql

--- src/main/java/JavaSparkSql.java
+++ src/main/java/JavaSparkSql.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 
-public final class JavaSparkConnect {
+public final class JavaSparkSql {
 
     public static void main(String[] args) {
         SparkSession spark = SparkSession
